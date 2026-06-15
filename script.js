efe06aa7285fc58f529e6ec4dcff6b9cde0b87ca
Fernanda Oliveira <fernandaoliveira.mep70@gmail.com>
2026-06-15 17:05:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const cards = document.querySelectorAll(
+'.service-card, .testimonial, .card-mini'
+);
+
+const observer = new IntersectionObserver(entries => {
+
+entries.forEach(entry => {
+
+if(entry.isIntersecting){
+
+entry.target.style.opacity = '1';
+entry.target.style.transform = 'translateY(0)';
+
+}
+
+});
+
+});
+
+cards.forEach(card => {
+
+card.style.opacity = '0';
+card.style.transform = 'translateY(40px)';
+card.style.transition = '0.8s';
+
+observer.observe(card);
+
+});
